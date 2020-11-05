9daf16084209e4cf9148da7e964588c9a0af4774
Vincent Tscherter <vincent@tscherter.net>
2020-11-05 21:02:04 +0100

    cleanup

---
+++ attic/mithril.js
@@ -0,0 +1,42 @@
+import {} from '//unpkg.com/mithril'
+import merge from '//unpkg.com/mergerino?module';;
+
+export default class Mithril extends HTMLElement {
+
+  constructor() {
+    super();
+    this.render();
+    Object.defineProperty(this, 'patch', {
+      value: (payload, detail) => {
+        const old = this.value
+        this.value = merge(this.value, payload)
+        this.dispatchEvent(new CustomEvent('input', {detail, old, new: this.value }))}})
+  }
+
+  get value () {
+    return this._value$ || { }
+  }
+
+  set value (value) {
+    Object.defineProperty(this, '_value$', { configurable: true, value })
+    this.render()
+  }
+
+  render() { m.render(this, m('div', [
+    this.value?.count || 0,
+    m('button', {
+      onclick: () => this.patch({ count: x => (x||0)+1 })
+    }, '+'),
+    m('input', {
+      placeholder: 'notes...',
+      value: this.value.note,
+      oninput: ({target}) => this.patch({note: target.value})
+    }),
+    `(${this.value?.note?.length ?? 0})`,
+    m('button', {
+      onclick: () => this.patch(()=>{})
+    }, 'reset')
+  ]))}
+}
+
+customElements.define("miruku-null-mithril", Mithril);
