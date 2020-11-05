30cceca910fb3d082802199416711a16d3d9eb9a
Vincent Tscherter <vincent@tscherter.net>
2020-11-05 20:34:54 +0100

    switch from state to value

--- mithril.js
+++ mithril.js
@@ -8,35 +8,33 @@ export default class Mithril extends HTMLElement {
     this.render();
     Object.defineProperty(this, 'patch', {
       value: (payload, detail) => {
-        const oldState = this.state
-        const newState = merge(this.state, payload)
-        console.log('patch', oldState, newState, detail)
-        this.state = newState
-        this.dispatchEvent(new CustomEvent('change', {detail, oldState, newState }))}})
+        const old = this.value
+        this.value = merge(this.value, payload)
+        this.dispatchEvent(new CustomEvent('change', {detail, old, new: this.value }))}})
   }
 
-  get state () {
-    return this._state$ || { }
+  get value () {
+    return this._value$ || { }
   }
 
-  set state (value) {
-    Object.defineProperty(this, '_state$', { configurable: true, value })
+  set value (value) {
+    Object.defineProperty(this, '_value$', { configurable: true, value })
     this.render()
   }
 
   render() { m.render(this, m('div', [
-    this.state?.count || 0,
+    this.value?.count || 0,
     m('button', {
       onclick: () => this.patch({ count: x => (x||0)+1 })
     }, '+'),
     m('input', {
       placeholder: 'notes...',
-      value: this.state.note,
+      value: this.value.note,
       oninput: ({target}) => this.patch({note: target.value})
     }),
-    `(${this.state?.note?.length ?? 0})`,
+    `(${this.value?.note?.length ?? 0})`,
     m('button', {
-      onclick: () => this.state = {}
+      onclick: () => this.value = {}
     }, 'reset')
   ]))}
 }
